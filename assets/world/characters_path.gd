90d0508ba087cfb3cc2dbd0c902e93d33b9691ec
Andrés López <andreslopez01182009@gmail.com>
2026-05-02 14:26:21 -0500

    Simplificación en el characters_path (2)
    Simplificación entre muchas comillas XDD
    Ahora usamos remote_transform para desacoplar los dos sistemas (personajes y caminos)

--- assets/world/characters_path.gd
+++ assets/world/characters_path.gd
@@ -1,5 +1,5 @@
 ## [code]CharacterPath[/code] gestiona la trayectoria de los personajes para que se muevan a lo
-## largo de una fila
+## largo de una fila según la entrada del jugador
 class_name CharactersPath extends Path2D
 
 
@@ -9,6 +9,10 @@ const PATH_MAX_LENGTH := 100.0
 const MIN_PATH_TRACE := 1.0
 ## Distancia entre personajes
 const CHARA_DISTANCE := 30.0
+
+
+## Nodo que contiene los personajes controlados por el jugador
+@export var characters_node: Node
 
 
 #region Preparación
@@ -21,35 +25,53 @@ func _ready() -> void:
 ## Encuentra a los personajes y los configura para ser seguidos
 func _find_and_set_characters() -> void:
 	# ? La estructura de la escena bajo este nodo debe ser:
-	# ? - CharactersPath: Path2D (este nodo)
-	# ?     - PathFollow
-	# ?         - Character: Character
-	# ?     - PathFollow2
-	# ?         - Character2: Character
-	# ?     - ...
-	# Busca a los personajes que haya y conecta la señal de actualización solo para el primero
-	var follower_found := false
-	for child in get_children():
-		var character: Character = child.get_child(0)
+	# ? - PathFollow
+	# ?     - RemoteTransform -> Apunta al personaje
+	# ? - PathFollow2
+	# ?     - RemoteTransform -> Apunta al personaje
+	# ? - ...
+	# Busca a los personajes (verificando que son los correctos)
+	var characters := characters_node.get_children().filter(func(c): return c is Character)
+	if characters.is_empty():
+		push_warning("[CharactersPath] No se encontraron personajes válidos en el nodo")
+		return
 
-		# El primer personaje en la lista será el lider
+	# Por cada personaje que no sea lider instancia un PathFollow, un RemoteTransform y empieza a asignar
+	var follower_found := false
+	for character: Character in characters:
+		# Forzamos a que el personaje ignore escalas/posiciones de sus padres
+		# Para evitar problemas de movimiento
+		character.top_level = true
+		character.is_following = true
+
+		# Designa al lider
 		if not follower_found:
 			follower_found = true
 			character.is_following = false
-			character.top_level = true # Evita que el líder sea arrastrado por el PathFollower
-			character.update_path.connect(func(new_pos: Vector2):
-				# Importante: El nodo CharactersPath NO debe ser hijo del Jugador
-				_on_update_path(new_pos)
-				_set_characters_pos()
-			)
+
+			# Conecta la señal para actualizar el path
+			character.update_path.connect(_on_update_path)
 			continue
 
-		# El resto sigue al lider
-		character.is_following = true
-
-		# Desactivamos colisiones para los seguidores para que no bloqueen al líder
+		# Desactiva la colisión para evitar problemas de movimiento
 		character.collision_layer = 0
 		character.collision_mask = 0
+
+		# Crea los nodos y añade al SceneTree
+		var path_follow := PathFollow2D.new()
+		var remote_transform := RemoteTransform2D.new()
+
+		add_child(path_follow)
+		path_follow.add_child(remote_transform)
+
+		# Configura el path_follow y el remote_transform
+		path_follow.rotates = false
+		path_follow.loop = false
+		remote_transform.update_rotation = false
+		remote_transform.update_scale = false
+
+		# Asignamos el personaje al remote_transform
+		remote_transform.remote_path = remote_transform.get_path_to(character)
 
 
 #endregion
@@ -66,6 +88,9 @@ func _on_update_path(new_pos: Vector2) -> void:
 	# Elimina el ultimo punto si la curva supera el largo máximo
 	if curve.get_baked_length() > PATH_MAX_LENGTH: curve.remove_point(0)
 
+	# Reubica los personajes
+	_set_characters_pos()
+
 
 ## Ubica a los personajes en una distancia equivalente a lo largo de la curva
 func _set_characters_pos() -> void:
@@ -73,20 +98,19 @@ func _set_characters_pos() -> void:
 	var baked_length := curve.get_baked_length()
 	if baked_length <= 0: return
 
-	var children := get_children()
-	for i in range(children.size()):
-		# Desactivamos el loop para evitar saltos
-		var character_follow: PathFollow2D = children[i]
-		character_follow.loop = false
+	# Buscamos a todos los personajes
+	for i in get_children().size():
+		var character_follow: PathFollow2D = get_child(i)
+		var remote_transform: RemoteTransform2D = character_follow.get_child(0)
 
 		# Obtenemos el personaje si es válido
-		var character: Character = character_follow.get_child(0)
+		var character: Character = remote_transform.get_node(remote_transform.remote_path)
 		if not character.is_following: continue
 
 		# Separamos a los personajes según CHARA_DISTANCE
 		# ? La distancia se cuenta en reversa porque el lider dibuja el final de la curva
 		# ? Usamos max(0) para que si el path es corto, esperen en el origen
-		var target_distance := baked_length - (i * CHARA_DISTANCE)
+		var target_distance := baked_length - (i + 1) * CHARA_DISTANCE
 		character_follow.progress = max(0.0, target_distance)
 
 
