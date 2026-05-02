a25e2bbd76789e21d1ef6f81089756c395b43cb3
Andrés López <andreslopez01182009@gmail.com>
2026-05-02 12:30:50 -0500

    Simplificación en el characters_path (1)

--- assets/world/characters_path.gd
+++ assets/world/characters_path.gd
@@ -11,6 +11,9 @@ const MIN_PATH_TRACE := 1.0
 const CHARA_DISTANCE := 30.0
 
 
+#region Preparación
+
+
 func _ready() -> void:
 	_find_and_set_characters()
 
@@ -19,9 +22,9 @@ func _ready() -> void:
 func _find_and_set_characters() -> void:
 	# ? La estructura de la escena bajo este nodo debe ser:
 	# ? - CharactersPath: Path2D (este nodo)
-	# ?     - PathFollower
+	# ?     - PathFollow
 	# ?         - Character: Character
-	# ?     - PathFollower2
+	# ?     - PathFollow2
 	# ?         - Character2: Character
 	# ?     - ...
 	# Busca a los personajes que haya y conecta la señal de actualización solo para el primero
@@ -29,21 +32,30 @@ func _find_and_set_characters() -> void:
 	for child in get_children():
 		var character: Character = child.get_child(0)
 
-		# El personaje lider, el primero, es el único que manda la señal
+		# El primer personaje en la lista será el lider
 		if not follower_found:
 			follower_found = true
+			character.is_following = false
 			character.top_level = true # Evita que el líder sea arrastrado por el PathFollower
 			character.update_path.connect(func(new_pos: Vector2):
 				# Importante: El nodo CharactersPath NO debe ser hijo del Jugador
 				_on_update_path(new_pos)
 				_set_characters_pos()
 			)
+			continue
+
 		# El resto sigue al lider
-		else:
-			character.is_following = true
-			# Desactivamos colisiones para los seguidores para que no bloqueen al líder
-			character.collision_layer = 0
-			character.collision_mask = 0
+		character.is_following = true
+
+		# Desactivamos colisiones para los seguidores para que no bloqueen al líder
+		character.collision_layer = 0
+		character.collision_mask = 0
+
+
+#endregion
+
+
+#region Actualización
 
 
 ## Actualiza la trayectoria de los personajes cuando reciba la señal de los jugadores
@@ -57,21 +69,25 @@ func _on_update_path(new_pos: Vector2) -> void:
 
 ## Ubica a los personajes en una distancia equivalente a lo largo de la curva
 func _set_characters_pos() -> void:
+	# Obtenemos la longitud de la curva
 	var baked_length := curve.get_baked_length()
 	if baked_length <= 0: return
 
 	var children := get_children()
-	# Empezamos desde el índice 1 (el primer seguidor)
-	for i in range(1, children.size()):
-		var character_follow: PathFollow2D = children[i]
+	for i in range(children.size()):
 		# Desactivamos el loop para evitar saltos
+		var character_follow: PathFollow2D = children[i]
 		character_follow.loop = false
 
+		# Obtenemos el personaje si es válido
 		var character: Character = character_follow.get_child(0)
-
 		if not character.is_following: continue
 
-		# Separamos a los personajes de forma equidistante
-		# Usamos max(0) para que si el path es corto, esperen en el origen
+		# Separamos a los personajes según CHARA_DISTANCE
+		# ? La distancia se cuenta en reversa porque el lider dibuja el final de la curva
+		# ? Usamos max(0) para que si el path es corto, esperen en el origen
 		var target_distance := baked_length - (i * CHARA_DISTANCE)
 		character_follow.progress = max(0.0, target_distance)
+
+
+#endregion
